af7a024bca0e7373af1ab0477bd472e7703bab58
Dante <125911503+Dante8885@users.noreply.github.com>
2024-12-07 13:55:14 +0200

    Add files via upload

---
+++ js/Portfolio.js
@@ -0,0 +1,46 @@
+// Toggle icon for the navbar
+
+let menuIcon = document.querySelector ('#menu-icon');
+let navbar = document.querySelector ('.navbar');
+
+menuIcon.onclick = () => {
+
+        menuIcon.classList.toggle('bx-x');
+        navbar.classList.toggle('active');
+
+}
+
+
+
+// Scroll sections
+let sections = document.querySelectorAll('section');
+let navLinks = document.querySelectorAll('header nav a');
+
+window.onscroll = () => {
+  sections.forEach(sec => {
+    let top = window.scrollY;
+    let offset = sec.offsetTop - 100;
+    let height = sec.offsetHeight;
+    let id = sec.getAttribute('id');
+
+    if (top >= offset && top < offset + height) {
+      // Active navbar links
+      navLinks.forEach(link => {
+        link.classList.remove('active');
+        // Fix the selector here
+        document.querySelector('header nav a[href*="' + id + '"]').classList.add('active');
+      });
+    }
+  });
+
+    // Sticky header
+
+  let header = document.querySelector('header');
+  header.classList.toggle('sticky', window.scrollY > 100);
+
+ //remove toggle icon and navbar when click navbar links ( scrool)
+
+ menuIcon.classList.remove('bx-x');
+ navbar.classList.remove('active');
+
+};
